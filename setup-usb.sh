9790c9fb5655d1ed16ab80899d123113be25202c
ramblehead <v.rybynok@gmail.com>
2023-08-10 15:22:53 +0100

    Checkpoint

--- setup-usb.sh
+++ setup-usb.sh
@@ -8,6 +8,13 @@ if (( $(id -u) != 0 )); then
   exit 1
 fi
 
+SDPATH="$(dirname "${BASH_SOURCE[0]}")"
+if [[ ! -d "${SDPATH}" ]]; then SDPATH="${PWD}"; fi
+SDPATH="$(cd "${SDPATH}" && pwd)"
+readonly SDPATH
+
+cd "${SDPATH}"; echo + cd "${PWD}"
+
 # /b/; Update /boot/config.txt with dwc2
 # /b/{
 
@@ -31,8 +38,9 @@ then
   CMD=(cp -f /boot/cmdline.txt /boot/cmdline.txt.setup-usb-bkp)
   echo + "${CMD[*]}" && "${CMD[@]}"
 
-  CMD=(sed -i -E)
+  CMD=(sed -iE)
   CMD+=("'s/^\(.*\s+rootwait\)\s+\(.*\)$/\1 modules-load=dwc2 \2/'")
+  CMD+=(/boot/cmdline.txt)
   echo + "${CMD[*]}" && eval "${CMD[*]}"
 fi
 
@@ -41,7 +49,8 @@ fi
 # /b/; Enable ssh
 # /b/{
 
-CMD=(touch ssh)
+echo
+CMD=(touch /boot/ssh)
 echo + "${CMD[*]}" && "${CMD[@]}"
 
 # /b/}
@@ -49,13 +58,46 @@ echo + "${CMD[*]}" && "${CMD[@]}"
 # /b/; Enable libcomposite module
 # /b/{
 
-if ! grep -qE '^\s*libcomposite\*$' /etc/modules
+if ! grep -qE '^\s*libcomposite\s*$' /etc/modules
 then
+  echo
   CMD=(cp -vf /etc/modules /etc/modules.setup-usb-bkp)
   echo + "${CMD[*]}" && "${CMD[@]}"
 
   CMD=('echo "libcomposite" >> /etc/modules')
   echo + "${CMD[*]}" && eval "${CMD[*]}"
 fi
+
+# /b/}
+
+# /b/; Configure systemd script that sets up USB Device Descriptor
+# /b/{
+
+if [[ -d /usr/local/stow ]]
+then
+  readonly STOW_NAME=rpi4-ethernet-over-usb
+
+  echo
+  CMD=(mkdir -p "/usr/local/stow/${STOW_NAME}")
+  echo + "${CMD[*]}" && "${CMD[@]}"
+
+  CMD=(cp -rvf root/* "/usr/local/stow/${STOW_NAME}")
+  echo + "${CMD[*]}" && "${CMD[@]}"
+
+  echo
+  cd /usr/local/stow; echo + cd "${PWD}"
+
+  echo
+  CMD=(stow -v "${STOW_NAME}")
+  echo + "${CMD[*]}" && "${CMD[@]}"
+else
+  echo
+  CMD=(cp -rvf root/* /usr/local)
+  echo + "${CMD[*]}" && "${CMD[@]}"
+fi
+
+echo
+CMD=(systemctl enable rpi4-usb.service)
+echo + "${CMD[*]}" && "${CMD[@]}"
 
 # /b/}
---
+++ teardown-usb.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+if (( $(id -u) != 0 )); then
+  echo "Please, run as root"
+  exit 1
+fi
+
+SDPATH="$(dirname "${BASH_SOURCE[0]}")"
+if [[ ! -d "${SDPATH}" ]]; then SDPATH="${PWD}"; fi
+SDPATH="$(cd "${SDPATH}" && pwd)"
+readonly SDPATH
+
+cd "${SDPATH}"; echo + cd "${PWD}"
+
+# /b/; Teardown systemd script that sets up USB Device Descriptor
+# /b/{
+
+echo
+CMD=('systemctl disable rpi4-usb.service ||:')
+echo + "${CMD[*]}" && eval "${CMD[*]}"
+
+readonly STOW_NAME=rpi4-ethernet-over-usb
+
+if [[ -d /usr/local/stow/${STOW_NAME} ]]
+then
+  echo
+  cd /usr/local/stow; echo + cd "${PWD}"
+
+  echo
+  CMD=(stow -vD "${STOW_NAME}")
+  echo + "${CMD[*]}" && "${CMD[@]}"
+
+  echo
+  CMD=(rm -rvf "${STOW_NAME}")
+  echo + "${CMD[*]}" && "${CMD[@]}"
+else
+  echo
+  CMD=(rm -vf /usr/local/bin/rpi4-usb.sh)
+  echo + "${CMD[*]}" && "${CMD[@]}"
+
+  CMD=(rm -vf /usr/local/lib/systemd/system/rpi4-usb.service)
+  echo + "${CMD[*]}" && "${CMD[@]}"
+fi
+
+# /b/}
+
+# /b/; Disable libcomposite module
+# /b/{
+
+if grep -qE '^\s*libcomposite\s*$' /etc/modules
+then
+  echo
+  CMD=(cp -vf /etc/modules /etc/modules.teardown-usb-bkp)
+  echo + "${CMD[*]}" && "${CMD[@]}"
+
+  echo
+  CMD=(sed -ziE)
+  CMD+=("'s/[^\n]*libcomposite\s*\n//g'")
+  CMD+=(/etc/modules)
+  echo + "${CMD[*]}" && eval "${CMD[*]}"
+fi
+
+# /b/}
+
+# /b/; Remove dwc2 from /boot/config.txt
+# /b/{
+
+if grep -qE '^\s*dtoverlay\s*=\s*dwc2\s*$' /boot/config.txt
+then
+  echo
+  CMD=(cp -vf /boot/config.txt /boot/config.txt.teardown-usb-bkp)
+  echo + "${CMD[*]}" && "${CMD[@]}"
+
+  echo
+  CMD=(sed -ziE)
+  CMD+=("'s/[^\n]*dtoverlay\s*=\s*dwc2\s*\n//g'")
+  CMD+=(/boot/config.txt)
+  echo + "${CMD[*]}" && eval "${CMD[*]}"
+fi
+
+# /b/}
+
+# # /b/; Remove dwc2 from /boot/cmdline.txt
+# # /b/{
+
+# if ! grep -qE 'modules-load=dwc2' /boot/cmdline.txt
+# then
+#   echo
+#   CMD=(cp -f /boot/cmdline.txt /boot/cmdline.txt.setup-usb-bkp)
+#   echo + "${CMD[*]}" && "${CMD[@]}"
+
+#   CMD=(sed -i -E)
+#   CMD+=("'s/^\(.*\s+rootwait\)\s+\(.*\)$/\1 modules-load=dwc2 \2/'")
+#   echo + "${CMD[*]}" && eval "${CMD[*]}"
+# fi
+
+# # /b/}
